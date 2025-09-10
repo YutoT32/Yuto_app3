5f759897ae7ff811671f6da1f83dea3ca49b5471
yuto <warabi2mochi@gmail.com>
2025-09-10 23:04:32 +0900

    make npm enviroment

--- app3.js
+++ app3.js
@@ -1,32 +1,8 @@
-<!DOCTYPE html>
-<HTML>
-    <HEAD>
-        <meta charset="utf-8">
-        <TITLE>SDEV</TITLE>
-        <link rel="stylesheet" href="stylesheet.css">
-    </HEAD>
-<BODY>
-    <header>
-        <div class="container">
-            <div class="header-left">
-                <img class="logo" src="#">
-            </div>
-            <div class="header-right">
-                <a href="cart&buy.html" class="cart">カート/購入</a>
-            </div>
-        </div>
-    </header>
-    <div class="top-wrapper">
+const express = require('express');
+const app = express();
 
-    </div>
-    <div class="medium-warapper">
+app.get('/top', (req, res) => {
+    res.render('top.ejs');
+});
 
-    </div>
-    <duv class="bottom-wrapper">
-
-    </duv>
-    <footer>
-
-    </footer>
-</BODY>
-</HTML>
+app.listen(3000);
