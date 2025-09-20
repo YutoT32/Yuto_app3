866d6bd88904355ae007cdd79ed77ea808c6f153
yuto <warabi2mochi@gmail.com>
2025-09-20 23:06:44 +0900

    make mysql settings

--- app3.js
+++ app3.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 
 app.use(express.static('public'));
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'SDEV',
+    password: 'password',
+    database: 'app3'
+});
+
 app.get('/top', (req, res) => {
     res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+    "SELECT * FROM items;",
+    (error, results) => {
+        console.log(results);
+        res.render('index.ejs');
+    }
 });
 
 app.listen(3000);
