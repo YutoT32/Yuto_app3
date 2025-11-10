942f6df8d7566f7e355082180533e9f6e3a1d0b4
yuto <warabi2mochi@gmail.com>
2025-11-10 18:01:03 +0900

    251110-HTMLで画面作り（途中）

--- app3.js
+++ app3.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
-
 const app = express();
 
+//publicフォルダを静的ファイルのルートとして指定
 app.use(express.static('public'));
 
+//データベースに接続するための設定
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'SDEV',
@@ -12,12 +13,27 @@ const connection = mysql.createConnection({
     database: 'app3'
 });
 
+
+//トップ画面（top.ejs）を表示するルーティング
 app.get('/', (req, res) => {
     res.render('top.ejs');
 });
 
+//一覧画面（index.ejs）を表示するルーティング
 app.get('/index', (req, res) => {
-    res.render('index.ejs');
+    connection.query(
+        "SELECT * FROM items",
+        (error, results) => {
+            res.render('index.ejs', {items: results});
+        }
+    );
 });
 
-app.listen(3000);
+app.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+//サーバーを起動
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
